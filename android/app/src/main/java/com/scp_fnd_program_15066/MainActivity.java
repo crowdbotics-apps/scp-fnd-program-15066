a04e66f2a4b788ddfbc065a1263d2f00957df6a9
Crowdbotics <team@crowdbotics.com>
2020-03-25 17:06:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/scp_fnd_program_15066/MainActivity.java
@@ -0,0 +1,15 @@
+package com.scp_fnd_program_15066;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "scp_fnd_program_15066";
+    }
+}
